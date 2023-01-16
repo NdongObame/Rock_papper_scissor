9f34fa0d579fe14ecfd360dd682a8dfdf7c8d914
NdongObame <Jeremiejoshua@gmail.com>
2023-01-16 21:02:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+
+Game_option = {1: "rock",
+               2: "paper",
+               3: "scissor"}
+
+John_score = 0
+Player_score = 0
+replay = "y"
+print("you'll face John in this game of Rock paper scissor")
+while replay == "y":
+    Computer_choice = random.randrange(1, 3)
+    John = Game_option[Computer_choice]
+    Player_choice = input("enter \nR = rock \nP = paper \nS = scissors\n you play :").lower()
+
+    while Player_choice != "r" and Player_choice != "p" and Player_choice != "s":
+        print("wrong input. try again")
+    # player chose rock
+    if Player_choice == "r" and Computer_choice == 1:
+        print("john played " + John + "\nIt's a tied")
+    elif Player_choice == "r" and Computer_choice == 2:
+        print("john played " + John + "\n John won")
+        John_score += 1
+    elif Player_choice == "r" and Computer_choice == 3:
+        print("john played " + John + "\nYou won")
+        Player_score += 1
+    # player chose paper
+    elif Player_choice == "p" and Computer_choice == 1:
+        print("john played " + John + "\nYou won")
+        Player_score += 1
+    elif Player_choice == "p" and Computer_choice == 2:
+        print("john played " + John + "\nIt's a tied")
+    elif Player_choice == "p" and Computer_choice == 3:
+        print("john played " + John + "\nJohn won")
+        John_score += 1
+    # player chose scissor
+    elif Player_choice == "s" and Computer_choice == 1:
+        print("john played " + John + "\nJohn won")
+        John_score += 1
+    elif Player_choice == "s" and Computer_choice == 2:
+        print("john played " + John + "\nYou won")
+        Player_score += 1
+    elif Player_choice == "s" and Computer_choice == 3:
+        print("john played " + John + "\nIt's a tied")
+    print("john: " + str(John_score) + ". player:" + str(Player_score))
+    replay = input("do you want to play again ?\ny or n :")
